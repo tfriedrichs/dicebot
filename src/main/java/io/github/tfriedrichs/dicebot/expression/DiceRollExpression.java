d19a2a1c452cf740f7ec49d639368dbb4aeb4bd7
Torben Friedrichs <torben.friedrichs@rwth-aachen.de>
2019-07-20 00:39:19 +0200

    Add builder for DiceRollExpression

--- src/main/java/io/github/tfriedrichs/dicebot/expression/DiceRollExpression.java
+++ src/main/java/io/github/tfriedrichs/dicebot/expression/DiceRollExpression.java
@@ -2,8 +2,11 @@ package io.github.tfriedrichs.dicebot.expression;
 
 import io.github.tfriedrichs.dicebot.DiceRoll;
 import io.github.tfriedrichs.dicebot.evaluator.DiceRollEvaluator;
+import io.github.tfriedrichs.dicebot.evaluator.SumEvaluator;
 import io.github.tfriedrichs.dicebot.modifier.DiceRollModifier;
 import io.github.tfriedrichs.dicebot.source.RandomSource;
+import io.github.tfriedrichs.dicebot.source.ThreadLocalRandomSource;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DiceRollExpression implements DiceExpression {
@@ -29,12 +32,68 @@ public class DiceRollExpression implements DiceExpression {
     @Override
     public DiceRoll roll() {
         int numberOfDice = this.numberOfDice.roll().evaluate();
+        if (numberOfDice < 0) {
+            throw new IllegalArgumentException("Number of dice must not be negative");
+        }
         int numberOfSides = this.numberOfSides.roll().evaluate();
+        if (numberOfSides < 0) {
+            throw new IllegalArgumentException("Number of sides must not be negative");
+        }
         int[] rolls = randomSource.get(numberOfDice, 1,
             numberOfSides + 1).toArray();
         for (DiceRollModifier modifier : modifiers) {
             rolls = modifier.modifyRoll(rolls, 1, numberOfSides + 1);
         }
         return new DiceRoll(evaluator, rolls);
+    }
+
+    public static class Builder {
+
+        private final List<DiceRollModifier> modifiers = new ArrayList<>();
+        private RandomSource randomSource = new ThreadLocalRandomSource();
+        private DiceExpression numberOfDice = new NumberExpression(1);
+        private DiceExpression numberOfSides = new NumberExpression(6);
+        private DiceRollEvaluator evaluator = new SumEvaluator();
+
+        public Builder withRandomSource(RandomSource randomSource) {
+            this.randomSource = randomSource;
+            return this;
+        }
+
+        public Builder withNumberOfDice(DiceExpression numberOfDice) {
+            this.numberOfDice = numberOfDice;
+            return this;
+        }
+
+        public Builder withNumberOfSides(DiceExpression numberOfSides) {
+            this.numberOfSides = numberOfSides;
+            return this;
+        }
+
+        public Builder withNumberOfDice(int numberOfDice) {
+            this.numberOfDice = new NumberExpression(numberOfDice);
+            return this;
+        }
+
+        public Builder withNumberOfSides(int numberOfSides) {
+            this.numberOfSides = new NumberExpression(numberOfSides);
+            return this;
+        }
+
+        public Builder withModifier(DiceRollModifier modifier) {
+            this.modifiers.add(modifier);
+            return this;
+        }
+
+        public Builder withEvaluator(DiceRollEvaluator evaluator) {
+            this.evaluator = evaluator;
+            return this;
+        }
+
+        public DiceRollExpression build() {
+            return new DiceRollExpression(randomSource, numberOfDice, numberOfSides, modifiers,
+                evaluator);
+        }
+
     }
 }
