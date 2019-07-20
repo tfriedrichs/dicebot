cd5dafca193b21a517bf0448af2daac47fdb72d4
Torben Friedrichs <torben.friedrichs@rwth-aachen.de>
2019-07-21 00:55:18 +0200

    Add some static factory methods for binary operators

--- src/main/java/io/github/tfriedrichs/dicebot/expression/DiceExpression.java
+++ src/main/java/io/github/tfriedrichs/dicebot/expression/DiceExpression.java
@@ -1,10 +1,27 @@
 package io.github.tfriedrichs.dicebot.expression;
 
+import io.github.tfriedrichs.dicebot.result.BinaryOperatorResult.Operator;
 import io.github.tfriedrichs.dicebot.result.DiceRollResult;
 
 
 public interface DiceExpression {
 
     DiceRollResult roll();
+
+    static DiceExpression plus(DiceExpression left, DiceExpression right) {
+        return new BinaryOperatorExpression(Operator.PLUS, left, right);
+    }
+
+    static DiceExpression minus(DiceExpression left, DiceExpression right) {
+        return new BinaryOperatorExpression(Operator.MINUS, left, right);
+    }
+
+    static DiceExpression times(DiceExpression left, DiceExpression right) {
+        return new BinaryOperatorExpression(Operator.TIMES, left, right);
+    }
+
+    static DiceExpression number(int value) {
+        return new NumberExpression(value);
+    }
 
 }
