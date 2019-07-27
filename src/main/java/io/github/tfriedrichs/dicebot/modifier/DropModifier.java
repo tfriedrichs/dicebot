904e7de1afbeaca7ebc01121f1a3a10c095fc046
Torben Friedrichs <torben.friedrichs@rwth-aachen.de>
2019-07-27 22:07:05 +0200

    Add drop modifier

---
+++ src/main/java/io/github/tfriedrichs/dicebot/modifier/DropModifier.java
@@ -0,0 +1,45 @@
+package io.github.tfriedrichs.dicebot.modifier;
+
+import io.github.tfriedrichs.dicebot.result.DiceRoll;
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class DropModifier implements DiceRollModifier {
+
+    private final Direction direction;
+    private final int numberOfDiceToDrop;
+
+    public DropModifier(Direction direction,
+                        int numberOfDiceToDrop) {
+        if (numberOfDiceToDrop < 0) {
+            throw new IllegalArgumentException("Number of dice to drop must be positive.");
+        }
+        this.direction = direction;
+        this.numberOfDiceToDrop = numberOfDiceToDrop;
+    }
+
+    @Override
+    public DiceRoll modifyRoll(DiceRoll roll, int min, int max) {
+        DiceRoll result = new DiceRoll(roll);
+        Stream<Integer> indices = IntStream.range(0, result.getRolls().length).boxed();
+        if (direction == Direction.HIGH) {
+            indices.sorted(
+                    Comparator.comparing((Integer index) -> result.getRolls()[index]).reversed())
+                    .limit(numberOfDiceToDrop)
+                    .forEach(index -> result.addMetaDataToRoll(index, DiceRoll.MetaData.DROPPED));
+        } else {
+            indices.sorted(Comparator.comparing(index -> result.getRolls()[index]))
+                    .limit(numberOfDiceToDrop)
+                    .forEach(index -> result.addMetaDataToRoll(index, DiceRoll.MetaData.DROPPED));
+        }
+
+        return result;
+    }
+
+    public enum Direction {
+        HIGH,
+        LOW
+    }
+}
---
+++ src/test/java/io/github/tfriedrichs/dicebot/modifier/DropModifierTest.java
@@ -0,0 +1,61 @@
+package io.github.tfriedrichs.dicebot.modifier;
+
+import io.github.tfriedrichs.dicebot.result.DiceRoll;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DropModifierTest {
+
+    @Test
+    void shouldDropAllDiceIfNumberTooDropIsHigherThanTotalDice() {
+        DiceRoll rolls = new DiceRoll(5, 1, 6);
+        DropModifier modifier = new DropModifier(DropModifier.Direction.HIGH, 6);
+        DiceRoll modified = modifier.modifyRoll(rolls, 0, 6);
+        assertTrue(IntStream.range(0, modified.getRolls().length)
+                .mapToObj(modified::getMetaDataForRoll)
+                .allMatch(metaData -> metaData.contains(DiceRoll.MetaData.DROPPED)));
+    }
+
+    @Test
+    void shouldDropNoDiceIfNumberToDropIsZero() {
+        DiceRoll rolls = new DiceRoll(5, 1, 6);
+        DropModifier modifier = new DropModifier(DropModifier.Direction.HIGH, 0);
+        DiceRoll modified = modifier.modifyRoll(rolls, 0, 6);
+        assertTrue(IntStream.range(0, modified.getRolls().length)
+                .mapToObj(modified::getMetaDataForRoll)
+                .noneMatch(metaData -> metaData.contains(DiceRoll.MetaData.DROPPED)));
+    }
+
+    @Test
+    void shouldThrowForNegativeNumberOfDiceToDrop() {
+        DiceRoll rolls = new DiceRoll(5, 1, 6);
+        assertThrows(IllegalArgumentException.class,
+                () -> new DropModifier(DropModifier.Direction.HIGH, -2));
+    }
+
+    @Test
+    void shouldDropHigherDiceIfDirectionHigh() {
+        DiceRoll rolls = new DiceRoll(5, 1, 6);
+        DropModifier modifier = new DropModifier(DropModifier.Direction.HIGH, 2);
+        DiceRoll modified = modifier.modifyRoll(rolls, 0, 6);
+        assertArrayEquals(rolls.getRolls(), modified.getRolls());
+        assertTrue(modified.getMetaDataForRoll(0).contains(DiceRoll.MetaData.DROPPED));
+        assertFalse(modified.getMetaDataForRoll(1).contains(DiceRoll.MetaData.DROPPED));
+        assertTrue(modified.getMetaDataForRoll(2).contains(DiceRoll.MetaData.DROPPED));
+    }
+
+    @Test
+    void shouldKeepLowerDiceIfDirectionLow() {
+        DiceRoll rolls = new DiceRoll(5, 1, 6);
+        DropModifier modifier = new DropModifier(DropModifier.Direction.LOW, 2);
+        DiceRoll modified = modifier.modifyRoll(rolls, 0, 6);
+        assertArrayEquals(rolls.getRolls(), modified.getRolls());
+        assertTrue(modified.getMetaDataForRoll(0).contains(DiceRoll.MetaData.DROPPED));
+        assertTrue(modified.getMetaDataForRoll(1).contains(DiceRoll.MetaData.DROPPED));
+        assertFalse(modified.getMetaDataForRoll(2).contains(DiceRoll.MetaData.DROPPED));
+    }
+
+}
